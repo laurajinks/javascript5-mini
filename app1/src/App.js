011bf9142f4fa17b667e5af5fa5da5936008685b
Laura <laura.jinks@gmail.com>
2019-01-08 15:17:44 -0600

    debug app 1

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
-  constuctor(props) {
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      myName: 'Joe'
+        this.state = {
+            myName: "Joe"
+        };
     }
-  }
-  render() {
-    return (
-      <div className="App">
-        My name is { this.state.myName }
-      </div>
-    );
-  }
+
+    render() {
+        return <div className="App">My name is {this.state.myName}</div>;
+    }
 }
 
 export default App;
