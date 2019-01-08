796c5e8373b0a7d1bd10f05247158f574149030d
Laura <laura.jinks@gmail.com>
2019-01-08 15:40:28 -0600

    debug app 4

--- app4/src/App.js
+++ app4/src/App.js
@@ -1,32 +1,44 @@
-import React, { Component } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cars: []
+    constructor(props) {
+        super(props);
+        this.state = {
+            cars: []
+        };
+
+        this.getCars = this.getCars.bind(this);
     }
-  }
 
-  getCars() {
-    axios.get('https://joes-autos.herokuapp.com/api/vehicles')
-    .then(res => {
-      this.setState({
-        cars: res
-      })
-    })
-  }
+    getCars() {
+        axios.get("https://joes-autos.herokuapp.com/api/vehicles").then(res => {
+            console.log(res.data);
+            this.setState({
+                cars: res.data
+            });
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <button onClick={this.getCars}>Get cars</button>
-        {this.state.cars}
-      </div>
-    );
-  }
+    render() {
+        const cars = this.state.cars.map(car => {
+            return (
+                <div>
+                    <h3>Make: {car.make}</h3>
+                    <h3>Model: {car.model}</h3>
+                    <p>Year: {car.year}</p>
+                    <p>Price: {car.price}</p>
+                </div>
+            );
+        });
+        return (
+            <div className="App">
+                <button onClick={this.getCars}>Get cars</button>
+                {cars}
+            </div>
+        );
+    }
 }
 
 export default App;
