8fe19e041488a36d6cdb995a269a26693e8f35c7
Laura <laura.jinks@gmail.com>
2019-01-08 15:32:57 -0600

    debug app 3

--- app3/src/App.js
+++ app3/src/App.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Movie from 'Movie';
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      badMovies: ['Battlerield Earth', 'Catwoman', 'The Last Airbender']
+    constructor(props) {
+        super(props);
+        this.state = {
+            badMovies: ["Battlerield Earth", "Catwoman", "The Last Airbender"]
+        };
     }
-  }
 
-  render() {
+    render() {
+        const badMovies = this.state.badMovies.map(movie => (
+            <Movie movie={movie} />
+        ));
 
-    const badMovies = this.state.badMovies.map( movie => {
-      <Movie movie={movie} />
-    })
-
-    return (
-      <div className="App">
-        <h1>Terrible Movies List:</h1>
-        [badMovies]
-      </div>
-    );
-  }
+        return (
+            <div className="App">
+                <h1>Terrible Movies List:</h1>
+                {badMovies}
+            </div>
+        );
+    }
 }
 
 export default App;
--- app3/src/Movie.js
+++ app3/src/Movie.js
@@ -1,7 +1,5 @@
-import React from 'react';
+import React from "react";
 
-export function Movie() {
-  return (
-      <p>{movie}</p>
-  )  
+export default function Movie(props) {
+    return <p>{props.movie}</p>;
 }
